8a79161eaa0a52ec5ecb2ca72e8f6e3822dd54d6
Daniel G. Taylor <danielgtaylor@gmail.com>
2015-07-08 15:33:55 +0200

    Add collapse nav support

--- templates/scripts.js
+++ templates/scripts.js
@@ -1,4 +1,8 @@
-function toggleCollapse(event) {
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
+function toggleCollapseButton(event) {
     var button = event.target.parentNode;
     var content = button.parentNode.nextSibling;
     var inner = content.children[0];
@@ -19,6 +23,29 @@ function toggleCollapse(event) {
     }
 }
 
+function toggleCollapseNav(event) {
+    var heading = event.target.parentNode;
+    var content = heading.nextSibling;
+    var inner = content.children[0];
+
+    if (heading.className.indexOf('heading') === -1) {
+      // Clicked without hitting the right element?
+      return;
+    }
+
+    if (content.style.maxHeight && content.style.maxHeight !== '0px') {
+      // Currently showing, so let's hide it, but only if this nav item
+      // is already selected. This prevents newly selected items from
+      // collapsing in an annoying fashion.
+      if (window.location.hash && endsWith(event.target.href, window.location.hash)) {
+        content.style.maxHeight = '0px';
+      }
+    } else {
+      // Currently hidden, so let's show it
+      content.style.maxHeight = inner.offsetHeight + 12 + 'px';
+    }
+}
+
 function refresh(body) {
     document.querySelector('body').className = 'preload';
     document.body.innerHTML = body;
@@ -33,12 +60,20 @@ function init() {
     // Make collapse buttons clickable
     var buttons = document.querySelectorAll('.collapse-button');
     for (var i = 0; i < buttons.length; i++) {
-        buttons[i].onclick = toggleCollapse;
+        buttons[i].onclick = toggleCollapseButton;
 
         // Show by default? Then toggle now.
         if (buttons[i].className.indexOf('show') !== -1) {
-            toggleCollapse({target: buttons[i].children[0]});
+            toggleCollapseButton({target: buttons[i].children[0]});
         }
+    }
+
+    var navItems = document.querySelectorAll('nav .resource-group .heading');
+    for (var i = 0; i < navItems.length; i++) {
+        navItems[i].onclick = toggleCollapseNav;
+
+        // TODO: Logic for auto-collapse.
+        toggleCollapseNav({target: navItems[i].children[0]});
     }
 }
 
